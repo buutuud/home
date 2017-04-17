1c425167ccebb63cc62ba9f6a86561f6eedd7a47
happyyi <buutuud@gmail.com>
2017-04-17 08:34:56 +0000

    add vimrc gitrc

---
+++ gitrc/README.md
@@ -0,0 +1,4 @@
+# gitrc
+my git config
+
+git自动化配置工具
---
+++ gitrc/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -sfv $PWD/gitconfig ~/.gitconfig
+ln -sfv $PWD/gitignore ~/.gitignore
+
+echo "Enter username:"
+read name
+echo "Enter email:"
+read email
+
+template="# vim:ft=gitconfig
+[user]
+        name = $name
+        email = $email"
+echo "$template" > ~/.gitconfig.local
